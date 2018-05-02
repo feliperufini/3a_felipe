6906a28177cde28e1c1fdf485293eecbb50d165d
Felipe Andrade <feliperufini01@gmail.com>
2018-05-02 11:13:44 -0400

    Inserção no Banco de Dados.

--- GrupoFelipe/src/br/edu/ifro/MenuPrincipalController.java
+++ GrupoFelipe/src/br/edu/ifro/MenuPrincipalController.java
@@ -5,13 +5,17 @@
  */
 package br.edu.ifro;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
@@ -38,5 +42,25 @@ public class MenuPrincipalController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void abrirCadastroAluno(ActionEvent event) {
+        try{
+    FXMLLoader fxmlLoader = new FXMLLoader();
+    fxmlLoader.setLocation(getClass().getResource("Aluno.fxml"));
+    Scene scene = new Scene(fxmlLoader.load(),400,300);
+    Stage stage = new Stage();
+    stage.setTitle("Cadastrar Cliente");
+    stage.setScene(scene);
+    stage.show();
+}
+catch(IOException e){
+
+}
+    }
+
+    @FXML
+    private void fecharProjeto(ActionEvent event) {
+    }
     
 }
